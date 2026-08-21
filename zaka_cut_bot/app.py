4c7a605d9c22d36b6d1798a715a674861a02e26d
firefox121 <artev270000@mail.ru>
2026-08-21 16:18:32 +0500

    Add files via upload

---
+++ zaka_cut_bot/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import threading
+import bot  # импортируем твой основной файл с ботом
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Бот запущен!"
+
+def run_bot():
+    # Запускаем бота в отдельном потоке, чтобы он не блокировал Flask
+    bot.bot.infinity_polling()
+
+if __name__ == "__main__":
+    # Запускаем бота в фоновом потоке
+    thread = threading.Thread(target=run_bot)
+    thread.start()
+    # Запускаем Flask сервер на порту, который дает Render
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
